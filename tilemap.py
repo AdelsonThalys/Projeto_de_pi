4afebc054b12fc4a28fdc3e914e5ff525a511f93
AdelsonThalys <68720308+AdelsonThalys@users.noreply.github.com>
2020-11-25 09:13:57 -0300

    Update tilemap.py

--- tilemap.py
+++ tilemap.py
@@ -1,5 +1,9 @@
 import pygame as pg
-from config import *
+import pytmx
+from settings import *
+
+def collide_hit_rect(one, two):
+    return one.hit_rect.colliderect(two.rect)
 
 class Map:
     def __init__(self, filename):
@@ -13,6 +17,28 @@ class Map:
         self.width = self.tilewidth * TILESIZE
         self.height = self.tileheight * TILESIZE
 
+class TiledMap:
+    def __init__(self, filename):
+        tm = pytmx.load_pygame(filename, pixelalpha=True)
+        self.width = tm.width * tm.tilewidth
+        self.height = tm.height * tm.tileheight
+        self.tmxdata = tm
+
+    def render(self, surface):
+        ti = self.tmxdata.get_tile_image_by_gid
+        for layer in self.tmxdata.visible_layers:
+            if isinstance(layer, pytmx.TiledTileLayer):
+                for x, y, gid, in layer:
+                    tile = ti(gid)
+                    if tile:
+                        surface.blit(tile, (x * self.tmxdata.tilewidth,
+                                            y * self.tmxdata.tileheight))
+
+    def make_map(self):
+        temp_surface = pg.Surface((self.width, self.height))
+        self.render(temp_surface)
+        return temp_surface
+
 class Camera:
     def __init__(self, width, height):
         self.camera = pg.Rect(0, 0, width, height)
@@ -22,9 +48,12 @@ class Camera:
     def apply(self, entity):
         return entity.rect.move(self.camera.topleft)
 
+    def apply_rect(self, rect):
+        return rect.move(self.camera.topleft)
+
     def update(self, target):
-        x = -target.rect.x + int(WIDTH / 2)
-        y = -target.rect.y + int(HEIGHT / 2)
+        x = -target.rect.centerx + int(WIDTH / 2)
+        y = -target.rect.centery + int(HEIGHT / 2)
 
         # limit scrolling to map size
         x = min(0, x)  # left
