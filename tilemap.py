a6a01a8aeb9fe86a3a4fa54676afc4c4d0717806
AdelsonThalys <68720308+AdelsonThalys@users.noreply.github.com>
2020-12-21 14:05:05 -0300

    Update tilemap.py

--- tilemap.py
+++ tilemap.py
@@ -1,9 +1,5 @@
 import pygame as pg
-import pytmx
 from settings import *
-
-def collide_hit_rect(one, two):
-    return one.hit_rect.colliderect(two.rect)
 
 class Map:
     def __init__(self, filename):
@@ -17,28 +13,6 @@ class Map:
         self.width = self.tilewidth * TILESIZE
         self.height = self.tileheight * TILESIZE
 
-class TiledMap:
-    def __init__(self, filename):
-        tm = pytmx.load_pygame(filename, pixelalpha=True)
-        self.width = tm.width * tm.tilewidth
-        self.height = tm.height * tm.tileheight
-        self.tmxdata = tm
-
-    def render(self, surface):
-        ti = self.tmxdata.get_tile_image_by_gid
-        for layer in self.tmxdata.visible_layers:
-            if isinstance(layer, pytmx.TiledTileLayer):
-                for x, y, gid, in layer:
-                    tile = ti(gid)
-                    if tile:
-                        surface.blit(tile, (x * self.tmxdata.tilewidth,
-                                            y * self.tmxdata.tileheight))
-
-    def make_map(self):
-        temp_surface = pg.Surface((self.width, self.height))
-        self.render(temp_surface)
-        return temp_surface
-
 class Camera:
     def __init__(self, width, height):
         self.camera = pg.Rect(0, 0, width, height)
@@ -48,16 +22,13 @@ class Camera:
     def apply(self, entity):
         return entity.rect.move(self.camera.topleft)
 
-    def apply_rect(self, rect):
-        return rect.move(self.camera.topleft)
-
     def update(self, target):
-        x = -target.rect.centerx + int(WIDTH / 2)
-        y = -target.rect.centery + int(HEIGHT / 2)
+        x = -target.rect.x + int(WIDTH / 2)
+        y = -target.rect.y + int(HEIGHT / 2)
 
         # limit scrolling to map size
-        x = min(0, x)  # left
-        y = min(0, y)  # top
-        x = max(-(self.width - WIDTH), x)  # right
-        y = max(-(self.height - HEIGHT), y)  # bottom
+        x = min(0 - 64, x)  # left
+        y = min(0 - 64, y)  # top
+        x = max(-(self.width - WIDTH - 64), x)  # right
+        y = max(-(self.height - HEIGHT - 64), y)  # bottom
         self.camera = pg.Rect(x, y, self.width, self.height)
