0ac0a6268092f5ce33201d268b90b4b50511f852
AdelsonThalys <68720308+AdelsonThalys@users.noreply.github.com>
2020-12-21 22:01:15 -0300

    Add files via upload

---
+++ código/main.py
@@ -0,0 +1,91 @@
+import pygame as pg
+import sys
+from os import path
+from settings import *
+from sprites import *
+from tilemap import *
+
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        self.load_data()
+
+    def load_data(self):
+        game_folder = path.dirname(__file__)
+        self.map = Map(path.join(game_folder, 'map.txt'))
+
+    def new(self):
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        self.background = pg.sprite.Group()
+        for row, tiles in enumerate(self.map.data):
+            for col, tile in enumerate(tiles):
+                if tile == '1':
+                    Wall(self, col, row)
+                if tile == 'P':
+                    self.player = Player(self, col, row)
+                # if tile == 'B':
+                #    Fundo()
+        self.camera = Camera(self.map.width, self.map.height)
+
+    def run(self):
+
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
+
+    def quit(self):
+        pg.quit()
+        sys.exit()
+
+    def update(self):
+        self.all_sprites.update()
+        self.camera.update(self.player)
+
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        bg_img = pg.image.load('img/fundo.jpg')
+        pg.transform.scale(bg_img, (WIDTH, HEIGHT))
+        self.screen.blit(bg_img, (0, 0))
+        self.draw_grid()
+        for sprite in self.all_sprites:
+            self.screen.blit(sprite.image, self.camera.apply(sprite))
+        pg.display.flip()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+
+    def show_start_screen(self):
+        pass
+
+    def show_go_screen(self):
+        pass
+
+
+# create the game object
+g = Game()
+g.show_start_screen()
+while True:
+    g.new()
+    g.run()
+    g.show_go_screen()
+    Dialogo()
